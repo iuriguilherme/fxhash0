22410134847ba312a928487532de620b60ccfe3b
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2023-02-02 20:53:25 -0300

    Ready for submission

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 /**!
- * @file fxhash-boilerplate-webpack-libre  
- * @version 0.0.0.0  
+ * @file Fibonacci Rainbow Spirals for fxhash.xyz  
+ * @version 1.0.0  
  * @copyright Iuri Guilherme 2023  
  * @license GNU AGPLv3  
  * @author Iuri Guilherme <https://iuri.neocities.org/>  
+ * @description This is Fibonacci Spirals with p5js for fxhash.xyz GT  
  * 
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU Affero General Public License as published by the 
@@ -21,52 +22,88 @@
  */
 
 import p5 from 'p5';
+import { create, all } from 'mathjs'
 
-const unique = fxhash
-const super = getFeatureSuper(fxrand())
+const math_config = { }
+const math = create(all, math_config)
 
-window.$fxhashFeatures = {
-  "super": super
-}
+const sqrt5 = math.sqrt(5);
+const spin = math.pi / 2;
+const maxIterations = 100;
+const maxSpirals = 360;
+const lastFibonacci = getFibonacci(maxIterations);
+const featureColor = getFeatureColor(fxhash);
+const featureHue = math.abs(featureColor % maxSpirals);
+const featureLuminance = math.min(60,
+  math.max(30,
+    math.abs(featureColor % maxIterations)
+  )
+);
+let size;
 
 let sketch = function(p5) {
   p5.setup = function() {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    p5.background(255);
-    p5.frameRate(30);
-  };
-  p5.draw = function() {
-    p5.background(255);
-    document.write("fxhash: " + unique)
-    document.write("super: " + super)
-    document.write("random: " + fxhash())
+    p5.randomSeed(fxrand() * 1e8);
+    size = p5.min(p5.windowWidth, p5.windowHeight);
+    p5.createCanvas(size, size);
+    p5.colorMode(p5.HSL)
+    p5.noLoop();
+};
+  p5.draw = async function() {
+    p5.background(featureHue, 90, featureLuminance);
+    p5.noFill();
+    p5.strokeWeight(1);
+    p5.translate(p5.windowWidth / 2, p5.windowHeight / 2);
+    for (var iteration = 0; iteration < maxIterations; iteration++) {
+      var fibonacci = getFibonacci(iteration);
+      for (var spiral = 0; spiral <= maxSpirals; spiral++) {
+        p5.stroke(
+          spiral,
+          math.abs(100 - iteration * (100 / maxIterations)),
+          math.abs(100 - featureLuminance)
+        );
+        p5.arc(
+          spiral,
+          iteration * spiral,
+          fibonacci * iteration,
+          fibonacci * spiral,
+          fibonacci,
+          spin
+        );
+        await new Promise(r => setTimeout(r, 1));
+      }
+      p5.rotate(spin);
+    }
+    fxpreview();
   };
   p5.windowResized = function() {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    size = p5.min(p5.windowWidth, p5.windowHeight);
+    p5.createCanvas(size, size);
   }
 }
 let myp5 = new p5(sketch, window.document.body);
 
-function getFeatureSuper(value) {
-  if (value < 0.1) {
-    return 0.1;
-  } else if (value < 0.2) {
-    return 0.2;
-  } else if (value < 0.3) {
-    return 0.3;
-  } else if (value < 0.4) {
-    return 0.4;
-  } else if (value < 0.5) {
-    return 0.5;
-  } else if (value < 0.6) {
-    return 0.6;
-  } else if (value < 0.7) {
-    return 0.7;
-  } else if (value < 0.8) {
-    return 0.8;
-  } else if (value < 0.9) {
-    return 0.9;
-  } else {
-    return 1.0;
-  }
+/**
+ * @param {int} n: nth fibonacci number in the fibonacci sequence
+ * @returns {float} a very good approximation of the nth fibonacci number
+*/
+function getFibonacci(n) {
+    return math.pow(math.phi, n) / sqrt5;
+}
+
+/**
+ * @param {String} hash: unique fxhash string
+ * @returns {int} number than can be used for value in color ranges
+ * @description Adapted from https://stackoverflow.com/a/66494926/7839535
+ */
+function getFeatureColor(hash) {
+    let unique = [...hash].reduce((acc, char) => {
+        return char.charCodeAt(0) + ((acc << 5) - acc);
+    }, 0);
+    return unique;
+}
+
+window.$fxhashFeatures = {
+  "Hue": featureHue,
+  "Luminance": featureLuminance
 }
